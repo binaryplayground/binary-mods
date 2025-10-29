e2cf3aa8a6115cc52225de550a2bd984d2b61ec0
findingsimple <jason@findingsimple.com>
2025-10-29 17:31:09 +1030

    Revert

--- includes/jetty.php
+++ includes/jetty.php
@@ -9,23 +9,6 @@ add_action( 'admin_head', function() {
     echo '<style>.edd-promo-notice--inactivepro{display:none!important;}</style>';
 }, 999 );
 
-// Remove the Passes component before it gets registered
-add_filter( 'edd_cron_components', function( $components_to_register ) {
-    // Remove the Passes component which handles Pro license checks
-    $key = array_search( 'EDD\Cron\Components\Passes', $components_to_register, true );
-    if ( $key !== false ) {
-        unset( $components_to_register[ $key ] );
-    }
-    
-    // Also remove by class reference (in case it's stored differently)
-    $components_to_register = array_filter( $components_to_register, function( $component ) {
-        return $component !== 'EDD\Cron\Components\Passes' && 
-               $component !== \EDD\Cron\Components\Passes::class;
-    });
-    
-    return $components_to_register;
-}, 1 );
-
 /**
  * Change login logo
  */
