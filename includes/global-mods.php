98a4e6ad2ba2edc7b474b9279f252a67d7b87a18
findingsimple <jason@findingsimple.com>
2025-10-31 12:15:35 +1030

    Filter singular and plural names only

--- includes/global-mods.php
+++ includes/global-mods.php
@@ -36,26 +36,11 @@ add_action( 'wp_enqueue_scripts', 'binarymods_custom_styles', PHP_INT_MAX );
 /**
  * Custom EDD labels
  */
-function binarymods_set_download_labels($labels) {
-    $labels = array(
-        'name' => _x('Products', 'post type general name', 'easy-digital-downloads'),
-        'singular_name' => _x('Product', 'post type singular name', 'easy-digital-downloads'),
-        'add_new' => __('Add New', 'easy-digital-downloads'),
-        'add_new_item' => __('Add New Product', 'easy-digital-downloads'),
-        'edit_item' => __('Edit Product', 'easy-digital-downloads'),
-        'new_item' => __('New Product', 'easy-digital-downloads'),
-        'all_items' => __('All Products', 'easy-digital-downloads'),
-        'view_item' => __('View Product', 'easy-digital-downloads'),
-        'search_items' => __('Search Products', 'easy-digital-downloads'),
-        'not_found' =>  __('No Products found', 'easy-digital-downloads'),
-        'not_found_in_trash' => __('No Products found in Trash', 'easy-digital-downloads'), 
-        'parent_item_colon' => '',
-        'menu_name' => __('Products', 'easy-digital-downloads'),
-        'featured_image'        => __( '%1$s Image', 'easy-digital-downloads' ),
-        'set_featured_image'    => __( 'Set %1$s Image', 'easy-digital-downloads' ),
-        'remove_featured_image' => __( 'Remove %1$s Image', 'easy-digital-downloads' ),
-        'use_featured_image'    => __( 'Use as %1$s Image', 'easy-digital-downloads' ),
-    );
-    return $labels;
+function binarymods_edd_product_labels( $labels ) {
+	$labels = array(
+	   'singular' => __('Product', 'easy-digital-downloads'),
+	   'plural'   => __('Products', 'easy-digital-downloads')
+	);
+	return $labels;
 }
-add_filter( 'edd_download_labels', 'binarymods_set_download_labels' );
+add_filter( 'edd_default_downloads_name', 'binarymods_edd_product_labels' );
